35954fcb223d48e367685a2b34fccc84c1ac53ef
lexa <linuxoid6666@gmail.com>
2016-03-23 23:35:34 +0200

    #add base vidget

---
+++ app/src/main/java/com/example/lexa/weatherclient/Widget.java
@@ -0,0 +1,10 @@
+package com.example.lexa.weatherclient;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by lexa on 23.03.16.
+ */
+public class Widget extends AppWidgetProvider {
+
+}
